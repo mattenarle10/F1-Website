e34f90d56696e28e9c769849e43befbba5d8c7f3
mattenarle10 <89822774+mattenarle10@users.noreply.github.com>
2022-06-28 19:47:18 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+/*=============== MATTHEW ENARLE JS F1 FILE ===============*/
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+    if(navToggle){
+        navToggle.addEventListener('click',() =>{
+            navMenu.classList.add('show-menu')
+        })
+    }
+
+    if(navClose){
+        navClose.addEventListener('click',() =>{
+            navMenu.classList.remove('show-menu')
+        })
+    }
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=> n.addEventListener('click',linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+    window.addEventListener('scroll', scrollHeader)
+
+/*=============== TEAMS SWIPER ===============*/
+let swiperTeam = new Swiper(".team__container", {
+    loop:true,
+    spaceBetween:24,
+    slidesPerView:'auto',
+    grabCursor:true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          spaceBetween: 48,
+        },
+      },
+  });
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay:400
+})
+
+sr.reveal('.home__title, .team__container, .drivers__filters')
+sr.reveal('.home__subtitle', {delay:500})
+sr.reveal('.home__elec', {delay:600})
+sr.reveal('.home__img', {delay:800})
+sr.reveal('.home__car-data', {delay:900, interval: 100, origin: 'bottom'})
+sr.reveal('.home__button', {delay:1000, interval: 100, origin: 'bottom'})
+sr.reveal('.about__group, .races__data', {origin: 'left'})
+sr.reveal('.about__data, .races__img', {origin: 'right'})
+sr.reveal('.drivers__card, .logo__content, .footer__content', {interval:100})
